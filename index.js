72269c146ed59e1f70cd2a4dd42418f6ba1ce1cd
Aparup Barua <roudra10jr@gmail.com>
2025-11-10 01:40:10 +0600

    successfully connected with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +8,37 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+	"mongodb+srv://ecopindbUser:lFgOdqiE10WimWR6@cluster0.cc5jffg.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
 app.get("/", (req, res) => {
 	res.send("EcoPin server is perfectly running");
 });
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
 	console.log(`EcoPin server is running on port: ${port}`);
